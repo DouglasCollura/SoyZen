1064ad592bdde5d1aeb6c4b3965cb59a399cde92
DouglasCollura <douglascollura@gmail.com>
2024-02-22 18:34:36 -0400

    card component

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, booleanAttribute } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatIconModule
+  ],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardComponent {
+
+  @Input({transform: booleanAttribute}) new:boolean = false;
+  @Input({required: true}) title!:string;
+  @Input({required: true}) url_img!:string;
+
+
+
+ }
