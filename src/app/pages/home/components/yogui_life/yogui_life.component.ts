5904a534d3b5f1404aaa767a56d232b94f835637
DouglasCollura <douglascollura@gmail.com>
2024-04-07 17:43:24 -0400

    update yogui_life

--- src/app/pages/home/components/yogui_life/yogui_life.component.ts
+++ src/app/pages/home/components/yogui_life/yogui_life.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 import { Roles } from '@services/auth.service';
 import { CardComponent } from '@shared/components/card/card.component';
 
@@ -10,7 +11,8 @@ import { CardComponent } from '@shared/components/card/card.component';
   imports: [
     CommonModule,
     MatIconModule,
-    CardComponent
+    CardComponent,
+    RouterModule
   ],
   templateUrl: './yogui_life.component.html',
   styleUrls: ['./yogui_life.component.scss','./yogui_life-mobile.component.scss'],
