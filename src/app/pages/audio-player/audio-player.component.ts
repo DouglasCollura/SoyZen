f6117ef825f87f42fb5e2a34b65efd28626c007c
DouglasCollura <douglascollura@gmail.com>
2024-04-21 11:44:03 -0400

    audioplayer get media

--- src/app/pages/audio-player/audio-player.component.ts
+++ src/app/pages/audio-player/audio-player.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, signal } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
 import { VgControlsModule } from '@videogular/ngx-videogular/controls';
 import { VgApiService, VgCoreModule } from '@videogular/ngx-videogular/core';
 import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-audio-player',
@@ -28,6 +29,8 @@ export default class AudioPlayerComponent {
 
   private vgPlayer:VgApiService | undefined;
   public isPlaying = signal(false);
+  @Input() urlPlayer:string = '';
+  public urlMedia = environment.urlMedia;
 
   public controlVideoPlayer = signal({
     isOver:false,
@@ -36,6 +39,12 @@ export default class AudioPlayerComponent {
 
 
   private timeOut:any;
+
+
+  getMedia(){
+    return `${this.urlMedia}${this.urlPlayer}`;
+  }
+
 
   onPlayerReady(api: VgApiService) {
     this.vgPlayer = api;
