de3a298dbdcca73437b8a435518eb0234c6f681a
Menphis <menphisj@gmail.com>
2024-05-06 15:41:16 -0400

    link footer

--- src/app/shared/components/layout/footer/footer.component.ts
+++ src/app/shared/components/layout/footer/footer.component.ts
@@ -13,4 +13,15 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrls: ['./footer.component.scss', 'footer-mobile.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent { }
+export class FooterComponent {
+  irweb(link:any){
+    if(link=='facebook'){
+      window.open("https://m.facebook.com/100092244648405/", "_blank");
+    }else if(link=='instagram'){
+      window.open("https://www.instagram.com/_soyzen?igsh=aTFjbmQ0OWM0MXZu", "_blank");
+    }else{
+      window.open("https://www.tiktok.com/@_soyzen?_t=8esHCIZpyDO&_r=1", "_blank");
+    }
+  }
+ }
+
